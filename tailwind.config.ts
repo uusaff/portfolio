52bede3a70b582eb34236d882d7e71eeeff52a77
uusaff <malikyousafanwar@gmail.com>
2026-07-19 19:21:42 +0500

    fixed a bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // Tell Tailwind to rely on the `.dark` class on the <html> tag for dark mode
-  darkMode: ["class"], 
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
